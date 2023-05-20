331739de8cc5e67dc7cf9aaab8a2eec0829240a4
Anmar Abbas <anmar@floranow.com>
2023-05-20 17:15:57 +0000

    Update query `1733108-stg_shipments`

--- 1. ERP Models/- Staging/~shipments/2104793-1733108-stg_shipments.sql
+++ 1. ERP Models/- Staging/~shipments/2104793-1733108-stg_shipments.sql
@@ -2,7 +2,7 @@ create or replace view `floranow.Floranow_ERP.stg_shipments` as
 
 with stg_shipments as 
 (
-    select
+select
 msh.id,
 sum(sh.total_quantity) as calc_total_quantity,
 count (distinct sh.id) as shipments_count,
