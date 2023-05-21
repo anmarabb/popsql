a4c018790c572e2e79ca9214255e0782a1209365
Anmar Abbas <anmar@floranow.com>
2023-05-21 06:48:51 +0000

    Update query `new_budget`

--- May 2023/Finance/budget/2105252-new_budget.sql
+++ May 2023/Finance/budget/2105252-new_budget.sql
@@ -17,7 +17,7 @@ WITH daily_budget AS (
 monthly_budget AS (
     SELECT 
         db.year_month,
-        SUM(db.daily_budget) over  AS total_monthly_budget
+        SUM(db.daily_budget) over AS total_monthly_budget
     FROM daily_budget db
     GROUP BY db.year_month
 )
