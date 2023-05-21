9eb05903b1d40b72968db96f07d37133a4f0a0b0
Anmar Abbas <anmar@floranow.com>
2023-05-21 05:10:25 +0000

    Update query `new_budget`

--- May 2023/Finance/budget/2105252-new_budget.sql
+++ May 2023/Finance/budget/2105252-new_budget.sql
@@ -18,7 +18,7 @@ SELECT
     d AS date,  
     db.daily_budget,
     db.budget,
-    max(db.budget) over (partition by )
+    max(db.budget) over (partition by db.year_month)
     db.financial_administration,
     db.account_manager,
     db.city,
