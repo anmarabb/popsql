f267f923d331f0a0d80ccb3dedcb4c8d579b225f
Anmar Abbas <anmar@floranow.com>
2023-05-21 04:26:49 +0000

    Update query `new_budget`

--- May 2023/Finance/budget/2105252-new_budget.sql
+++ May 2023/Finance/budget/2105252-new_budget.sql
@@ -8,7 +8,6 @@ WITH daily_budget AS (
         b.date,
         b.client_category,
         PARSE_DATE('%Y-%m-%d', CONCAT(FORMAT_TIMESTAMP('%Y-%m', b.date), '-01')) as year_month,
-
         b.budget,
         b.budget / CAST(DATETIME_DIFF(DATETIME_ADD(DATETIME_TRUNC(b.date, MONTH), INTERVAL 1 MONTH), DATETIME_TRUNC(b.date, MONTH), DAY) AS FLOAT64) AS daily_budget,
 
