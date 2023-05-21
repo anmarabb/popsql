d87bc6d73447855aca9069dec97290461541a496
Anmar Abbas <anmar@floranow.com>
2023-05-21 07:10:02 +0000

    Update query `new_budget`

--- May 2023/Finance/budget/2105252-new_budget.sql
+++ May 2023/Finance/budget/2105252-new_budget.sql
@@ -1,7 +1,7 @@
---create or replace view `floranow.Floranow_ERP.budget` as
+create or replace view `floranow.Floranow_ERP.budget` as
 
 WITH daily_budget AS (
-        SELECT
+    SELECT 
         b.financial_administration,
         b.account_manager,
         b.city,
@@ -12,16 +12,12 @@ WITH daily_budget AS (
         b.budget / CAST(DATETIME_DIFF(DATETIME_ADD(DATETIME_TRUNC(b.date, MONTH), INTERVAL 1 MONTH), DATETIME_TRUNC(b.date, MONTH), DAY) AS FLOAT64) AS daily_budget,
         GENERATE_DATE_ARRAY(DATE(DATETIME_TRUNC(b.date, MONTH)), DATE(DATETIME_SUB(DATETIME_ADD(DATETIME_TRUNC(b.date, MONTH), INTERVAL 1 MONTH), INTERVAL 1 DAY))) AS date_range
     FROM `floranow.erp_prod.budget` as b
-
-)
-
 )
 SELECT 
     db.year_month,
     d AS date,  
     db.daily_budget,
     db.budget,
-    mb.total_monthly_budget,   -- new field
     db.financial_administration,
     db.account_manager,
     db.city,
@@ -29,9 +25,6 @@ SELECT
     DATETIME_DIFF(DATE(DATETIME_SUB(DATETIME_ADD(DATETIME_TRUNC(d,MONTH), INTERVAL 1 MONTH), INTERVAL 1 DAY)),DATE_TRUNC(d,MONTH),DAY)+1 as days_total,
     DATETIME_DIFF(DATETIME_SUB(DATETIME_ADD(DATETIME_TRUNC(d,MONTH), INTERVAL 1 MONTH), INTERVAL 1 DAY),d,DAY) as days_remaining,
     DATETIME_DIFF(d,DATE_TRUNC(d,MONTH),DAY) as days_passed
+    
 FROM daily_budget db
 JOIN UNNEST(db.date_range) AS d
-LEFT JOIN monthly_budget mb
-ON db.year_month = mb.year_month
-
-where financial_administration = 'UAE' and db.year_month = '2023-05-01'
