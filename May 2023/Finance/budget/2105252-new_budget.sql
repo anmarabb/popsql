f8de2d23270aefeb25e10c0358db1f622c2d7524
Anmar Abbas <anmar@floranow.com>
2023-05-22 07:10:12 +0000

    Update query `new_budget`

--- May 2023/Finance/budget/2105252-new_budget.sql
+++ May 2023/Finance/budget/2105252-new_budget.sql
@@ -10,23 +10,30 @@ WITH daily_budget AS (
         PARSE_DATE('%Y-%m-%d', CONCAT(FORMAT_TIMESTAMP('%Y-%m', b.date), '-01')) as year_month,
         b.budget,
         b.budget / CAST(DATETIME_DIFF(DATETIME_ADD(DATETIME_TRUNC(b.date, MONTH), INTERVAL 1 MONTH), DATETIME_TRUNC(b.date, MONTH), DAY) AS FLOAT64) AS daily_budget,
-        GENERATE_DATE_ARRAY(DATE(DATETIME_TRUNC(b.date, MONTH)), DATE(DATETIME_SUB(DATETIME_ADD(DATETIME_TRUNC(b.date, MONTH), INTERVAL 1 MONTH), INTERVAL 1 DAY))) AS date_range
+        GENERATE_DATE_ARRAY(DATE(DATETIME_TRUNC(b.date, MONTH)), DATE(DATETIME_SUB(DATETIME_ADD(DATETIME_TRUNC(b.date, MONTH), INTERVAL 1 MONTH), INTERVAL 1 DAY))) AS date_range,
+
     FROM `floranow.erp_prod.budget` as b
 )
 SELECT 
     db.year_month,
     d AS date,  
     db.daily_budget,
-    db.budget,
     db.financial_administration,
     db.account_manager,
     db.city,
     db.client_category,
+
+    CASE
+        WHEN ROW_NUMBER() OVER (PARTITION BY DATE_TRUNC(date, MONTH)) = 1 THEN 
+            SUM(db.daily_budget) OVER (PARTITION BY DATE_TRUNC(date, MONTH))
+        ELSE 
+            NULL 
+    END AS monthly_budget,
+
     DATETIME_DIFF(DATE(DATETIME_SUB(DATETIME_ADD(DATETIME_TRUNC(d,MONTH), INTERVAL 1 MONTH), INTERVAL 1 DAY)),DATE_TRUNC(d,MONTH),DAY)+1 as days_total,
     DATETIME_DIFF(DATETIME_SUB(DATETIME_ADD(DATETIME_TRUNC(d,MONTH), INTERVAL 1 MONTH), INTERVAL 1 DAY),d,DAY) as days_remaining,
     DATETIME_DIFF(d,DATE_TRUNC(d,MONTH),DAY) as days_passed,
-
-    case when date_diff(date(db.year_month) , current_date() , MONTH) = 0 then db.daily_budget else 0 end as MTD_budget,
     
 FROM daily_budget db
 JOIN UNNEST(db.date_range) AS d
+where financial_administration = 'UAE' and year_month = '2023-05-01' and city = 'Al Ain'
