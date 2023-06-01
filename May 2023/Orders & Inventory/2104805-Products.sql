6c74efdcce4a4781acc7ada079b2f219915d16c9
Anmar Abbas <anmar@floranow.com>
2023-06-01 06:33:41 +0000

    Update query `Products`

--- May 2023/Orders & Inventory/2104805-Products.sql
+++ May 2023/Orders & Inventory/2104805-Products.sql
@@ -35,9 +35,9 @@ p.remaining_quantity,
 p.quantity,
 p.quantity as p_quantity,
 
-CAST(p.unit_landed_cost AS FLOAT64) as unit_landed_cost
 
---p.remaining_quantity * p.unit_price as remaining_value,
+
+p.remaining_quantity * p.unit_price as remaining_value,
 p.remaining_quantity * p.unit_landed_cost as remaining_value,
 date_diff(cast(p.expired_at as date) , cast(p.updated_at as date), day) - date_diff(cast(current_date() as date), cast (p.updated_at as date) , day) as Remaining_Age,
 
