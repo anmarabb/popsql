72938222c3957402ff92031e2e288b4542de75af
Anmar Abbas <anmar@floranow.com>
2023-06-05 09:28:39 +0000

    Update query `2. issue_link`

--- Jira/2129247-2. issue_link.sql
+++ Jira/2129247-2. issue_link.sql
@@ -22,4 +22,5 @@ LEFT JOIN `floranow.floranow_jira.user` AS creator_account ON issue.creator_acco
 LEFT JOIN `floranow.floranow_jira.issue` AS parent_issue ON issue.parent_issue_id = parent_issue.id
 LEFT JOIN `floranow.floranow_jira.issue_link` AS issue_link ON issue.id = issue_link.issue_id
 group by issue.id
+having 
 ;
