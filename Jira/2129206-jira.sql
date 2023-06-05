31e844e2995b530b7339a30ed5ad0bece0c213ff
Anmar Abbas <anmar@floranow.com>
2023-06-05 09:01:52 +0000

    Update query `jira`

--- Jira/2129206-jira.sql
+++ Jira/2129206-jira.sql
@@ -21,7 +21,10 @@ LEFT JOIN `floranow.floranow_jira.project` AS project ON issue.project_id = proj
 LEFT JOIN `floranow.floranow_jira.user` AS assignee_account ON issue.assignee_account_id = assignee_account.account_id
 LEFT JOIN `floranow.floranow_jira.user` AS creator_account ON issue.creator_account_id = creator_account.account_id
 LEFT JOIN `floranow.floranow_jira.issue` AS parent_issue ON issue.parent_issue_id = parent_issue.id
-LEFT JOIN prepared_labels ON issue.id = prepared_labels.issue_id;
+LEFT JOIN prepared_labels ON issue.id = prepared_labels.issue_id
+
+
+;
 where issue.id='15884'
 
 
