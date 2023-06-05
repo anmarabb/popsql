67ccf8140112805b06f9ba09b5645dd4ba6531f4
Anmar Abbas <anmar@floranow.com>
2023-06-05 10:47:33 +0000

    Update query `2. issue_link`

--- Jira/2129247-2. issue_link.sql
+++ Jira/2129247-2. issue_link.sql
@@ -127,7 +127,7 @@ aggregated_issue_link AS (
   SELECT
     issue_link.issue_id,
     STRING_AGG(linked_issue_id) as linked_issue_ids,
-    STRING_AGG(linked_issue.summary) as linked_issues,
+    STRING_AGG(linked_issue.summary, '\n') as linked_issues,
     STRING_AGG(linked_issue.key) as linked_issue_keys,
 
      
