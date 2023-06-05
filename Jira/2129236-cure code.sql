4db438f678781e3ab9d755a12c6e802b277b1f14
Anmar Abbas <anmar@floranow.com>
2023-06-05 09:17:07 +0000

    Update query `cure code`

--- Jira/2129236-cure code.sql
+++ Jira/2129236-cure code.sql
@@ -9,8 +9,5 @@ LEFT JOIN `floranow.floranow_jira.user` AS creator_account ON issue.creator_acco
 
 LEFT JOIN `floranow.floranow_jira.issue` AS parent_issue on  issue.parent_issue_id = parent_issue.id
 
-LEFT JOIN `floranow.floranow_jira.issue_label` AS issue_label ON issue.id = issue_label.issue_id
-
-where issue.id='15884'
 
 ;
