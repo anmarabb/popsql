94638602c2a9bfb7f00364007b58a2546f0643a4
Anmar Abbas <anmar@floranow.com>
2023-06-05 14:49:20 +0000

    Update query `floranow_jira`

--- Jira/2129367-floranow_jira.sql
+++ Jira/2129367-floranow_jira.sql
@@ -81,3 +81,6 @@ LEFT JOIN `floranow.floranow_jira.issue` AS parent_issue on  issue.parent_issue_
 
 LEFT JOIN aggregated_labels ON issue.id = aggregated_labels.issue_id
 LEFT JOIN aggregated_issue_link ON issue.id = aggregated_issue_link.issue_id
+
+
+where roject.name ='Product 2.0'
