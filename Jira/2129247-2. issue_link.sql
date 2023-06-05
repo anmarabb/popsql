de3d869d595980bcf6b22981fa5be1adb4c76908
Anmar Abbas <anmar@floranow.com>
2023-06-05 10:39:52 +0000

    Update query `2. issue_link`

--- Jira/2129247-2. issue_link.sql
+++ Jira/2129247-2. issue_link.sql
@@ -130,7 +130,7 @@ aggregated_issue_link AS (
   FROM `floranow.floranow_jira.issue_link` as issue_link
   LEFT JOIN `floranow.floranow_jira.issue_link_type` AS issue_link_type ON issue_link.issue_link_type_id = issue_link_type.id 
   LEFT JOIN `floranow.floranow_jira.issue` AS linked_issue ON linked_issue.id = issue_link.linked_issue_id
-LEFT JOIN `floranow.floranow_jira.project` AS linked_issue_project ON linked_issue.project_id = linked_issue_project.id
+    LEFT JOIN `floranow.floranow_jira.project` AS linked_issue_project ON linked_issue.project_id = linked_issue_project.id
 
   GROUP BY 1
 
