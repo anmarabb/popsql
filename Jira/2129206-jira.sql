fa3a60ee08b21fb04709f16f35e6725638c0a03e
Anmar Abbas <anmar@floranow.com>
2023-06-05 09:01:32 +0000

    Update query `jira`

--- Jira/2129206-jira.sql
+++ Jira/2129206-jira.sql
@@ -15,7 +15,7 @@ WITH prepared_labels AS (
   GROUP BY issue_id
 )
 SELECT 
-  count(*),
+  prepared_labels
 FROM `floranow.floranow_jira.issue` AS issue 
 LEFT JOIN `floranow.floranow_jira.project` AS project ON issue.project_id = project.id
 LEFT JOIN `floranow.floranow_jira.user` AS assignee_account ON issue.assignee_account_id = assignee_account.account_id
