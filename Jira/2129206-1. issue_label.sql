030eeebc96a8c8baaf9619e2d4cb3d9f0b25a7f4
Anmar Abbas <anmar@floranow.com>
2023-06-05 09:55:00 +0000

    Update query `1. issue_label`

--- Jira/2129206-1. issue_label.sql
+++ Jira/2129206-1. issue_label.sql
@@ -68,34 +68,3 @@ where issue.id='15884'
 GROUP BY issue.id
 
 ;
-
-
-SELECT
-issue.id as issue_id,
-issue.created,
-project.name as project,
-issue.issue_type_name as issue_type,
-issue.summary as issue,
-issue.description,
-
-parent_issue.summary as parent_issue,
-parent_issue.description as parent_description,
-
-
-issue.status_name,
-issue.custom_epic_name,
-issue.key as issue_key,
-assignee_account.display_name as  assignee_to,
-creator_account.display_name as created_by,
-concat ('https://floranow.atlassian.net/browse/',issue.key) as issue_link,
-
-  STRING_AGG(issue_label.label) as labels,
-
-FROM `floranow.floranow_jira.issue` AS issue 
-LEFT JOIN `floranow.floranow_jira.project` AS project ON issue.project_id = project.id
-LEFT JOIN `floranow.floranow_jira.user` AS assignee_account ON issue.assignee_account_id = assignee_account.account_id
-LEFT JOIN `floranow.floranow_jira.user` AS creator_account ON issue.creator_account_id = creator_account.account_id
-
-LEFT JOIN `floranow.floranow_jira.issue` AS parent_issue on  issue.parent_issue_id = parent_issue.id
-
-LEFT JOIN `floranow.floranow_jira.issue_label` AS issue_label ON issue.id = issue_label.issue_id
