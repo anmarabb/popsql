24e42c5943e4923dcc6594081a9ff6421dfc0454
Anmar Abbas <anmar@floranow.com>
2023-06-05 09:17:05 +0000

    Update query `cure code`

---
+++ Jira/2129236-cure code.sql
@@ -0,0 +1,16 @@
+SELECT
+
+issue_label.*,
+
+FROM `floranow.floranow_jira.issue` AS issue 
+LEFT JOIN `floranow.floranow_jira.project` AS project ON issue.project_id = project.id
+LEFT JOIN `floranow.floranow_jira.user` AS assignee_account ON issue.assignee_account_id = assignee_account.account_id
+LEFT JOIN `floranow.floranow_jira.user` AS creator_account ON issue.creator_account_id = creator_account.account_id
+
+LEFT JOIN `floranow.floranow_jira.issue` AS parent_issue on  issue.parent_issue_id = parent_issue.id
+
+LEFT JOIN `floranow.floranow_jira.issue_label` AS issue_label ON issue.id = issue_label.issue_id
+
+where issue.id='15884'
+
+;
