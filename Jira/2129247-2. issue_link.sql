208a964786424bb666593e5fbb7501d5aa00d0d0
Anmar Abbas <anmar@floranow.com>
2023-06-05 09:36:13 +0000

    Update query `2. issue_link`

--- Jira/2129247-2. issue_link.sql
+++ Jira/2129247-2. issue_link.sql
@@ -41,6 +41,7 @@ having count(issue.id)>3
 
 SELECT
 issue_link.*
+
 FROM `floranow.floranow_jira.issue` AS issue 
 LEFT JOIN `floranow.floranow_jira.project` AS project ON issue.project_id = project.id
 LEFT JOIN `floranow.floranow_jira.user` AS assignee_account ON issue.assignee_account_id = assignee_account.account_id
