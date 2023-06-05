0eec56eb153721b93070af138dc93ba66ee7c743
Anmar Abbas <anmar@floranow.com>
2023-06-05 10:52:57 +0000

    Update query `2. issue_link`

--- Jira/2129247-2. issue_link.sql
+++ Jira/2129247-2. issue_link.sql
@@ -163,6 +163,7 @@ concat ('https://floranow.atlassian.net/browse/',issue.key) as issue_link,
 aggregated_labels.labels,
 
 
+
 FROM `floranow.floranow_jira.issue` AS issue 
 LEFT JOIN `floranow.floranow_jira.project` AS project ON issue.project_id = project.id
 LEFT JOIN `floranow.floranow_jira.user` AS assignee_account ON issue.assignee_account_id = assignee_account.account_id
