97886bdc22ab89385872aad459c9d63fc6707d1e
Anmar Abbas <anmar@floranow.com>
2023-06-08 09:59:06 +0000

    Update query `Users`

--- May 2023/Users/2104778-Users.sql
+++ May 2023/Users/2104778-Users.sql
@@ -214,7 +214,7 @@ stg_invoice_items_customer_id.wtd_holland,
 stg_invoice_items_customer_id.w_1_holland,
 stg_invoice_items_customer_id.w_2_holland,
 stg_invoice_items_customer_id.w_3_holland,
-stg_invoice_items_customer_id.w_1_holland,
+stg_invoice_items_customer_id.w_4_holland,
 
 
 case 
