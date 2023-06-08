bd9c905e23b4e1c37ac84e741d800785f7e750f9
Anmar Abbas <anmar@floranow.com>
2023-06-08 10:13:23 +0000

    Update query `stg_invoice_items_customer_id`

--- May 2023/Users/2104789-stg_invoice_items_customer_id.sql
+++ May 2023/Users/2104789-stg_invoice_items_customer_id.sql
@@ -16,7 +16,7 @@ select
 SUM (
   CASE 
     WHEN 
-      TIMESTAMP_TRUNC(i.printed_at, WEEK(MONDAY)) = current_date(TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL 1 WEEK), WEEK(MONDAY)) 
+      current_date(i.printed_at, WEEK(MONDAY)) = current_date(TIMESTAMP_SUB(current_date(), INTERVAL 1 WEEK), WEEK(MONDAY)) 
       AND li_suppliers.supplier_region = 'Holland' 
     THEN ii.price_without_tax 
     ELSE 0 
