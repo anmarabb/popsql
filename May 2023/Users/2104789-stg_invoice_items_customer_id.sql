4cc394969f47ea5c3e2c43bd7bde2175664aed49
Anmar Abbas <anmar@floranow.com>
2023-06-08 09:55:54 +0000

    Update query `stg_invoice_items_customer_id`

--- May 2023/Users/2104789-stg_invoice_items_customer_id.sql
+++ May 2023/Users/2104789-stg_invoice_items_customer_id.sql
@@ -15,7 +15,7 @@ select
 
 sum (case when date_diff(current_date(),date(i.printed_at), WEEK) = 0 and li_suppliers.supplier_region = 'Holland' then ii.price_without_tax else 0 end) as last_week_holland,
 sum (case when date_diff(current_date(),date(i.printed_at), WEEK) = 1 and li_suppliers.supplier_region = 'Holland' then ii.price_without_tax else 0 end) as last_week_holland,
-sum (case when date_diff(current_date(),date(i.printed_at), WEEK) = 1 and li_suppliers.supplier_region = 'Holland' then ii.price_without_tax else 0 end) as last_week_holland,
+sum (case when date_diff(current_date(),date(i.printed_at), WEEK) = 2 and li_suppliers.supplier_region = 'Holland' then ii.price_without_tax else 0 end) as last_week_holland,
 
 --ii.order_date,
 --ii.printed_at,
