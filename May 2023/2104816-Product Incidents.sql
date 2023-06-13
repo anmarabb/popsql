bfdbad9be294b7a4c47f44e580cd2b14d413afe7
Anmar Abbas <anmar@floranow.com>
2023-06-13 07:36:49 +0000

    Update query `Product Incidents`

--- May 2023/2104816-Product Incidents.sql
+++ May 2023/2104816-Product Incidents.sql
@@ -101,7 +101,7 @@ case when li.order_type = 'OFFLINE' and orr.standing_order_id is not null then '
 
 fs.name as feed_source,
 
-case when  w.name is null then 'fro'
+case when  w.name is null then 'from order'
 
 stock_id,
 stock.name as stock,
