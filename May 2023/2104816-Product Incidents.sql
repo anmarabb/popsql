ee995b8298ecfc451eb1dd6e21f25e84eee32143
Anmar Abbas <anmar@floranow.com>
2023-06-13 07:40:25 +0000

    Update query `Product Incidents`

--- May 2023/2104816-Product Incidents.sql
+++ May 2023/2104816-Product Incidents.sql
@@ -104,7 +104,9 @@ fs.name as feed_source,
 
 
 case 
-when pi.stage = 'INVENTORY' and pi.incident_type = 'DAMAGED' and w.name is null then 'dmage from order' else 'dmage from inventory' end as damage_type,
+when pi.stage = 'INVENTORY' and pi.incident_type = 'DAMAGED' and w.name is null then 'dmage from order' 
+
+else 'dmage from inventory' end as damage_type,
 
 
 
