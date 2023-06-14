f99cf54abf3837b66eb3a74977f4493e8a4159a6
Anmar Abbas <anmar@floranow.com>
2023-06-14 22:31:36 +0000

    Update query `stg_invoices`

--- May 2023/Users/2104784-stg_invoices.sql
+++ May 2023/Users/2104784-stg_invoices.sql
@@ -112,7 +112,6 @@ when round(SAFE_DIVIDE(sum(i.total_amount), date_diff(cast(Max(i.printed_at) as 
 when round(SAFE_DIVIDE(sum(i.total_amount), date_diff(cast(Max(i.printed_at) as date), cast(Min(i.printed_at) as date ), MONTH)),0) <1000 then "7- Clients who pay less than 999 per month"
 when date_diff(cast(Max(i.printed_at) as date), cast(Min(i.printed_at) as date ), MONTH) = 0 then 'One order clinets'
 when date_diff(cast(Max(i.printed_at) as date), cast(Min(i.printed_at) as date ), MONTH) is null then 'Zero order clinets'
-
 else 'One order clinets'
 
 end as client_value_segments,
