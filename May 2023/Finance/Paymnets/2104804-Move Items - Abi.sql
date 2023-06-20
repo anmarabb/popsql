1957150a95bcf94186ae700b8d2782405eb6060c
Anmar Abbas <anmar@floranow.com>
2023-06-20 07:27:52 +0000

    Update query `Move Items - Abi`

--- May 2023/Finance/Paymnets/2104804-Move Items - Abi.sql
+++ May 2023/Finance/Paymnets/2104804-Move Items - Abi.sql
@@ -2,7 +2,8 @@ create or replace table `floranow.Floranow_ERP.payments` as
 
     
     select date(cmi.date) as cridet_date, -- date of paymnet_transaction
-    
+    case when w.name is not null then w.name  end as warehouse,
+
            customer.warehouse_id as customer_warehouse_id,
            date(pt.payment_received_at)                                    payment_received_at,
            customer.name                                                   customer,
