a85d7038ba076db5836d442a5999781c0387eb1b
Anmar Abbas <anmar@floranow.com>
2023-06-22 12:04:07 +0000

    Imported 3 queries from PopSQL to GitHub

---
+++ Private queries/Database/BigQuery/2156863-BigQuery - invoices.sql
@@ -0,0 +1,15 @@
+SELECT
+    count(invoices.number) as row_count,
+FROM
+    `floranow.erp_prod.invoices` as invoices -- dmi.residual, dmi.balance 
+    JOIN floranow.erp_prod.users AS customers ON customers.id = invoices.customer_id
+    join floranow.erp_prod.move_items cmi on cmi.documentable_type = 'Invoice'
+    and cmi.documentable_id = invoices.id
+    and cmi.entry_type = 'CREDIT'
+WHERE
+    customers.warehouse_id = 79 -- Qassim
+    and customers.odoo_code like 'BQ-%'
+    and invoices.status in (1, 3, 6)
+    and invoices.invoice_type = 1
+    and invoices.printed_at >= '2023-05-01'
+    and invoices.printed_at < '2023-06-01'
