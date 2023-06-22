d26610634112b4ad55e14f48b9529475c7c1c86d
Anmar Abbas <anmar@floranow.com>
2023-06-22 11:14:18 +0000

    Update query `BigQuery - invoice`

--- Production Database/2156863-BigQuery - invoice.sql
+++ Production Database/2156863-BigQuery - invoice.sql
@@ -1,5 +1,5 @@
 SELECT
-    countinvoices.number
+    count(invoices.number
 FROM
     `floranow.erp_prod.invoices` as invoices -- dmi.residual, dmi.balance 
     JOIN floranow.erp_prod.users AS customers ON customers.id = invoices.customer_id
