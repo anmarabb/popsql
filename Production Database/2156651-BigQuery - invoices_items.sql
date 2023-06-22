42a76fad64e3bede01c3bd3394217cac33039925
Anmar Abbas <anmar@floranow.com>
2023-06-22 10:24:32 +0000

    Update query `BigQuery - invoices_items`

--- Production Database/2156651-BigQuery - invoices_items.sql
+++ Production Database/2156651-BigQuery - invoices_items.sql
@@ -1,7 +1,7 @@
 SELECT
 
 count(distinct i.id) as row_coun,
-max(i.printed_at) 
+max(i.printed_at) as 
 
 from `floranow.erp_prod.invoice_items`  as ii 
 left join `floranow.Floranow_ERP.users` as stg_users on stg_users.id = ii.customer_id
