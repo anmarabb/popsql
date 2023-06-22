307598423c2b1c15ae1ec2a225c58f3ab45b896b
Anmar Abbas <anmar@floranow.com>
2023-06-22 12:01:28 +0000

    Move query `postgres - Invoices` (part 2/2)

---
+++ Database/PostgreSQL/2156989-postgres - Invoices.sql
@@ -0,0 +1,19 @@
+-- Table: invoices
+-- number of Credit Note for May 2023
+
+
+SELECT
+    count(invoices.number) as row_count
+FROM
+    invoices -- dmi.residual, dmi.balance 
+    JOIN users AS customers ON customers.id = invoices.customer_id
+    join move_items cmi on cmi.documentable_type = 'Invoice'
+    and cmi.documentable_id = invoices.id
+    and cmi.entry_type = 'CREDIT'
+WHERE
+    customers.warehouse_id = 79 -- Qassim
+    and customers.odoo_code like 'BQ-%'
+    and invoices.status in (1, 3, 6)
+    and invoices.invoice_type = 1
+    and invoices.printed_at >= '2023-05-01'
+    and invoices.printed_at < '2023-06-01';
