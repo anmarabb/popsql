3eaee58d17a0a900688beb951a8873fe18b9015f
Anmar Abbas <anmar@floranow.com>
2023-07-05 08:01:54 +0000

    Update query `vendor_publishing`

--- Dashbaord Project/2114601-vendor_publishing.sql
+++ Dashbaord Project/2114601-vendor_publishing.sql
@@ -83,7 +83,7 @@ left join `floranow.vendor_portal_prod.accounts` as a on g.account_id= a.id
 
 left join `floranow.Floranow_ERP.suppliers` as s on s.floranow_supplier_id = a.floranow_account_id
 
-where ot.name not like '%Event%'
+--where ot.name not like '%Event%'
 
 --left join `floranow.vendor_portal_prod.specifications` as spec on spec.specifiable_id = p.id
 --left join `floranow.vendor_portal_prod.specification_values` as specV on specV.specification_id = spec.id
