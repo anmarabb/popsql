6c4ef0d05d1663c85bd2d671b4707f04411cdade
Anmar Abbas <anmar@floranow.com>
2023-07-05 12:03:01 +0000

    Update query `products`

--- Dashbaord Project/Orders & Inventory/2104805-products.sql
+++ Dashbaord Project/Orders & Inventory/2104805-products.sql
@@ -346,7 +346,7 @@ case
     when p.product_name like '%Wooden%' THEN 'Accessories'
     when p.product_name like '%Wrapping%' THEN 'Accessories'
     
-else p.category end as new_category,
+else p.category end as new_item_category,
 
 
 
