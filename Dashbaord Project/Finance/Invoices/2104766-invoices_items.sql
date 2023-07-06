29b0ed75c837b208fef732b3e027d5fa877e2272
Anmar Abbas <anmar@floranow.com>
2023-07-06 09:05:49 +0000

    Update query `invoices_items`

--- Dashbaord Project/Finance/Invoices/2104766-invoices_items.sql
+++ Dashbaord Project/Finance/Invoices/2104766-invoices_items.sql
@@ -24,7 +24,7 @@ case when w.name is not null then w.name  end as warehouse,
 
     case 
         when stg_users.customer_type = 'reseller' and stg_users.warehouse_id in (10,43,76,79) then 'remote branch reseller'
-        when stg_users.customer_type = 'reseller' 
+        when stg_users.customer_type = 'reseller' then 
 
 
         else stg_users.customer_type
