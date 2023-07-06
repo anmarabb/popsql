2ae5a5b7fea463aa7671471912e49ba47191fd27
Anmar Abbas <anmar@floranow.com>
2023-07-06 08:50:50 +0000

    Update query `invoices_items`

--- Dashbaord Project/Finance/Invoices/2104766-invoices_items.sql
+++ Dashbaord Project/Finance/Invoices/2104766-invoices_items.sql
@@ -8,7 +8,7 @@ SELECT
 
 case when w.name is not null then w.name  end as warehouse,
 
-case when stg_users.customer_type = 'reseller' and 
+case when stg_users.customer_type = 'reseller' and stg_users.warehouse_id 
 
 
 --stg_users
