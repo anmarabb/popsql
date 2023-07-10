472e56a3af15fcadd909918379fb60c225b1d690
Anmar Abbas <anmar@floranow.com>
2023-07-10 12:54:35 +0000

    Update query `products`

--- materialize/2183993-products.sql
+++ materialize/2183993-products.sql
@@ -349,6 +349,7 @@ else p.category end as new_item_category,
 
 
 from products as p
+
 left join stocks as st on st.id = p.stock_id and  st.reseller_id = p.reseller_id
 left join line_items as li on li.id = p.line_item_id
 left join product_locations as pl on pl.locationable_id = p.id and pl.locationable_type = 'Product'
