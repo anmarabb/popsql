dd7fca46b712473a104fd23405f78a2525072da6
Anmar Abbas <anmar@floranow.com>
2023-07-10 12:31:32 +0000

    Update query `products`

--- materialize/2183993-products.sql
+++ materialize/2183993-products.sql
@@ -1,6 +1,6 @@
 drop source erp_prod cascade;
 
-sections, suppliers, account_managers, manageable_accounts
+, suppliers, account_managers, manageable_accounts
 
 CREATE SOURCE erp_prod
   IN CLUSTER ingest_postgres
