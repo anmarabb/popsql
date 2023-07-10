5428842555c8bdb49f392f042f7b96c3e0be7eab
Anmar Abbas <anmar@floranow.com>
2023-07-10 12:49:35 +0000

    Update query `products`

--- materialize/2183993-products.sql
+++ materialize/2183993-products.sql
@@ -10,6 +10,9 @@ CREATE SOURCE erp_prod
 
 
 select count (*) as row_count
+
+
+
 from products as p
 left join stocks as st on st.id = p.stock_id and  st.reseller_id = p.reseller_id
 left join line_items as li on li.id = p.line_item_id
