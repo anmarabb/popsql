c6daf222e62df9f78ffcf0bcc10445949032bad0
Anmar Abbas <anmar@floranow.com>
2023-07-10 12:12:08 +0000

    Update query `products`

--- materialize/2183993-products.sql
+++ materialize/2183993-products.sql
@@ -2,7 +2,7 @@ drop source mz_source cascade;
 
 
 
-CREATE SOURCE mz_source
+CREATE SOURCE er
   IN CLUSTER ingest_postgres
   FROM POSTGRES CONNECTION pg_connection (PUBLICATION 'mz_source')
   FOR TABLES (stocks, users, products, locations, warehouses, line_items, feed_sources, order_requests, picking_products, product_locations, additional_items_reports, sections, suppliers, account_managers, manageable_accounts);
