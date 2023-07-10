83714c9ca29e5907c001389f607406eed078d5ca
Anmar Abbas <anmar@floranow.com>
2023-07-10 12:07:50 +0000

    Update query `products`

--- materialize/2183993-products.sql
+++ materialize/2183993-products.sql
@@ -1,4 +1,2 @@
 select count (*) as row_count
 from products as p
-left join stocks as st on st.id = p.stock_id and  st.reseller_id = p.reseller_id
-left join line_items as li on li.id = p.line_item_id
