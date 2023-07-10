493fa9d5cd2eb643d93957bbde3e262d2c2dece8
Anmar Abbas <anmar@floranow.com>
2023-07-10 12:40:52 +0000

    Update query `products`

--- materialize/2183993-products.sql
+++ materialize/2183993-products.sql
@@ -25,7 +25,7 @@ left join  line_items as parent_li on parent_li.id = li.parent_line_item_id
 
 
 left join suppliers as stg_suppliers on stg_suppliers.id = p.supplier_id
-left join floranow.Floranow_ERP.suppliers as li_suppliers on li_suppliers.id = li.supplier_id
+left join suppliers as li_suppliers on li_suppliers.id = li.supplier_id
 
 
 left join feed_sources as fs on fs.id = p.origin_feed_source_id
