21ae249055aa9b3dd78fede3913d22e91ea9ffa9
Anmar Abbas <anmar@floranow.com>
2023-07-10 12:39:04 +0000

    Update query `products`

--- materialize/2183993-products.sql
+++ materialize/2183993-products.sql
@@ -13,7 +13,7 @@ select count (*) as row_count
 from products as p
 left join stocks as st on st.id = p.stock_id and  st.reseller_id = p.reseller_id
 left join line_items as li on li.id = p.line_item_id
---left join product_locations as pl on pl.locationable_id = p.id and pl.locationable_type = "Product"
+left join product_locations as pl on pl.locationable_id = p.id and pl.locationable_type = "Product"
 --left join locations as loc on pl.location_id=loc.id
 
 
