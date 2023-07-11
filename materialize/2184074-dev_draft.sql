f5febefdf66d741269c5f8ad73d4c073b14ec369
Anmar Abbas <anmar@floranow.com>
2023-07-11 12:10:54 +0000

    Update query `dev_draft`

--- materialize/2184074-dev_draft.sql
+++ materialize/2184074-dev_draft.sql
@@ -6,3 +6,7 @@ CREATE SOURCE erp_prod
   IN CLUSTER ingest_postgres
   FROM POSTGRES CONNECTION pg_connection (PUBLICATION 'mz_source')
   FOR TABLES (sections, suppliers, account_managers, manageable_accounts, stocks, users, products, locations, warehouses, line_items, feed_sources, order_requests, picking_products, product_locations, additional_items_reports);
+
+
+
+set cluster = mz_introspection;
