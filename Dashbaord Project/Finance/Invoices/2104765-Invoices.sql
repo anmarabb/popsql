9d8dc71792b1d5dc1fc109b96aa33042c8bcf7e6
Anmar Abbas <anmar@floranow.com>
2023-07-18 10:20:23 +0000

    Update query `Invoices`

--- Dashbaord Project/Finance/Invoices/2104765-Invoices.sql
+++ Dashbaord Project/Finance/Invoices/2104765-Invoices.sql
@@ -25,7 +25,7 @@ sum(case when date(i.printed_at) is not null then i.remaining_amount else 0 end)
 sum(i.remaining_amount) over() as total_outstanding_with_proforma,
 
 
-datei.deleted_at,
+date(i.deleted_at,
 
 
 -- date
