11255b994a1b324e6bcc54eceebf464ad3ca72f7
Anmar Abbas <anmar@floranow.com>
2023-07-19 14:24:42 +0000

    Update query `Invoices`

--- Dashbaord Project/Finance/Invoices/2104765-Invoices.sql
+++ Dashbaord Project/Finance/Invoices/2104765-Invoices.sql
@@ -168,7 +168,7 @@ left join `floranow.Floranow_ERP.users` as stg_users on stg_users.id = i.custome
 left join prep_registered_clients as prep_registered_clients on prep_registered_clients.financial_administration = stg_users.financial_administration
 
 left join `floranow.Floranow_ERP.stg_paymnets` as stg_paymnets on stg_paymnets.invoice_id = i.id
-left join `floranow.Floranow_ERP.stg_invoice_items` as stg_invoice_items on stg_invoice_items.invoice_id = i.id
+left join `floranow.Floranow_ERP.stg_invoice_items` as stg_invoice_items on stg_invoice_items.invoice_id = i.id;
 
 
 
