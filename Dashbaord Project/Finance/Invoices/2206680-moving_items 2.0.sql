3e7f46d757fcaee4a4ead026f437a7b11a30f700
Anmar Abbas <anmar@floranow.com>
2023-07-24 22:22:52 +0000

    Update query `moving_items 2.0`

--- Dashbaord Project/Finance/Invoices/2206680-moving_items 2.0.sql
+++ Dashbaord Project/Finance/Invoices/2206680-moving_items 2.0.sql
@@ -24,7 +24,7 @@ case when entry_type = 'CREDIT' then balance else 0 end as total_credits,
 
 
 case when entry_type = 'DEBIT' then balance else 0 end as total_debits,
-case when entry_type = 'DEBIT' then balance else 0 end as total_debits,
+case when entry_type = 'DEBIT' and  then balance else 0 end as total_debits,
 
 
 case when entry_type = 'CREDIT' then residual else 0 end as unreconciled_credits,
