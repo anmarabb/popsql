d50c44a08e1c874404b033324bef03780e71b6ee
Anmar Abbas <anmar@floranow.com>
2023-07-24 15:51:54 +0000

    Update query `moving_items 2.0`

--- Dashbaord Project/Finance/Invoices/2206680-moving_items 2.0.sql
+++ Dashbaord Project/Finance/Invoices/2206680-moving_items 2.0.sql
@@ -49,6 +49,6 @@ and customer.financial_administration_id = 1
 and mi.balance != 0
 and ((mi.entry_type = 'DEBIT' AND round(residual, 2) >= 0) OR (mi.entry_type = 'CREDIT' AND round(mi.residual, 2) <= 0))
 
-and customer.id=965
+--and customer.id=965
 
 order by mi.date desc
