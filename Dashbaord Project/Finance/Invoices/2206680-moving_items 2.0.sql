7a2cb36388db7bda084916dafc85b0a2ab25ef97
Anmar Abbas <anmar@floranow.com>
2023-07-24 16:04:54 +0000

    Update query `moving_items 2.0`

--- Dashbaord Project/Finance/Invoices/2206680-moving_items 2.0.sql
+++ Dashbaord Project/Finance/Invoices/2206680-moving_items 2.0.sql
@@ -7,7 +7,7 @@ customer.name,
 case 
 when customeru.company_id = 3 then 'Bloomax Flowers LTD'
 when customer.company_id = 2 then 'Global Floral Arabia tr'
-when u.company_id = 1 then 'Flora Express Flower Trading LLC'
+when customer.company_id = 1 then 'Flora Express Flower Trading LLC'
 else  'cheack'
 end as company_name,
 
