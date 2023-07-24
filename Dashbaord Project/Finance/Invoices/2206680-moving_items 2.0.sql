d24c13a74ec8ce205e802c809469299d2033a09f
Anmar Abbas <anmar@floranow.com>
2023-07-24 22:23:31 +0000

    Update query `moving_items 2.0`

--- Dashbaord Project/Finance/Invoices/2206680-moving_items 2.0.sql
+++ Dashbaord Project/Finance/Invoices/2206680-moving_items 2.0.sql
@@ -24,6 +24,8 @@ case when entry_type = 'CREDIT' then balance else 0 end as total_credits,
 
 
 case when entry_type = 'DEBIT' then balance else 0 end as total_debits,
+
+
 case when entry_type = 'CREDIT' and mi.documentable_type = 'PaymentTransaction' then balance else 0 end as paymnets,
 
 
