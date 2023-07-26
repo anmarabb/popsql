ca7d43f8d0981992c04774f5f2c510696aa32908
Anmar Abbas <anmar@floranow.com>
2023-07-26 11:34:05 +0000

    Update query `invoices_items`

--- Dashbaord Project/Finance/Invoices/2104766-invoices_items.sql
+++ Dashbaord Project/Finance/Invoices/2104766-invoices_items.sql
@@ -499,6 +499,8 @@ li.parent_line_item_id,
 case when stg_users.customer in ('REMA1','REMA2','REMA3','REMA4','REMA5','REMA6','REMA7','REMA8') then 'Astra Shop' else 'null' end as astra_shop,
 
 
+
+
 from `floranow.erp_prod.invoice_items`  as ii 
 left join `floranow.Floranow_ERP.users` as stg_users on stg_users.id = ii.customer_id
 left join `floranow.erp_prod.line_items` as li on ii.line_item_id = li.id
