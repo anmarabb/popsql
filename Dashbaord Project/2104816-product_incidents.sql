8d5a07cf859e5c11ec5f3b443de81ea9650f603c
Anmar Abbas <anmar@floranow.com>
2023-07-28 12:36:01 +0000

    Update query `product_incidents`

--- Dashbaord Project/2104816-product_incidents.sql
+++ Dashbaord Project/2104816-product_incidents.sql
@@ -3,7 +3,7 @@ create or replace table `floranow.Floranow_ERP.product_incidents` as
 select 
 
 
-concat ('NCR-',li.departure_date,msh.id) as n
+concat ('NCR-',li.departure_date,msh.id) as N
 
 
 pi.incident_type, --MISSING, DAMAGED, EXTRA, RETURNED, QUALITY_ISSUES, TRANSACTIONAL_ISSUES, INCORRECT_ITEM
