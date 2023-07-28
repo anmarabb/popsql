b21f1daa478bda015f7b6bd8419db7c118114eed
Anmar Abbas <anmar@floranow.com>
2023-07-28 12:33:19 +0000

    Update query `product_incidents`

--- Dashbaord Project/2104816-product_incidents.sql
+++ Dashbaord Project/2104816-product_incidents.sql
@@ -76,7 +76,7 @@ li.currency,
 li.fob_currency,
 li.departure_date,
 
-concat ('NCR',li.departure_date)
+concat ('NCR-',li.departure_date)
 
 
 --LEAD(pi.created_at) OVER(PARTITION BY pi.line_item_id ORDER BY  pi.line_item_id,pi.created_at) as lead_created_at,
