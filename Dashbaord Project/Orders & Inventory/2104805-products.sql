6e491032a04fce9daaeb94919ee57322f2666785
Anmar Abbas <anmar@floranow.com>
2023-07-28 14:34:57 +0000

    Update query `products`

--- Dashbaord Project/Orders & Inventory/2104805-products.sql
+++ Dashbaord Project/Orders & Inventory/2104805-products.sql
@@ -164,7 +164,6 @@ when st.id in (613,614,615) then 'Internal - BX Shop'
 when st.id in (616,617) then 'Internal - Wedding & Events'
 when st.id in (621) then 'Internal - BX DMM'
 
-
 when st.id in (522,484,567,566,531,530) then 'Reselling'
 else 'Reselling'
 end as stock_model,
