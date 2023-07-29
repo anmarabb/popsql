ebcdc1645f48b5f0b727e1979807608ed06db4fa
Anmar Abbas <anmar@floranow.com>
2023-07-29 18:47:29 +0000

    Update query `test`

--- 2215626-test.sql
+++ 2215626-test.sql
@@ -1,5 +1,5 @@
 /*
-
+This SQL script calculates weekly user-level metrics for a transactional business. It calculates the number of transactions each user created in a given week, as well as over their lifetime up to that week. It also uses these numbers to create flags for new, churned, reactivated, and retained users.
 */
 
 
