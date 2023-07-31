aa79b4b99cbcf0ad79a3d84f04013258a7516141
Anmar Abbas <anmar@floranow.com>
2023-07-31 16:24:50 +0000

    Update query `moving_items 2.0`

--- Dashbaord Project/Finance/Invoices/2206680-moving_items 2.0.sql
+++ Dashbaord Project/Finance/Invoices/2206680-moving_items 2.0.sql
@@ -80,7 +80,7 @@ case
 			when mi.source_system in('FLORANOW_ERP', 'FLORISOFT') then u.company_id 
 		end
 	end
-	when u.warehouse_id not in(10, 79, 76, 43) or u.warehouse_id is null then
+	when customer.warehouse_id not in(10, 79, 76, 43) or u.warehouse_id is null then
 	case
 		when mi.source_system = 'ODOO' then 3
 		when mi.source_system in('FLORANOW_ERP', 'FLORISOFT') then u.company_id 
