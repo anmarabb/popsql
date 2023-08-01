eea865484cf4a8763c5ada3185dbca07ed7e7b66
Anmar Abbas <anmar@floranow.com>
2023-08-01 15:52:08 +0000

    Update query `moving_items 2.0`

--- Dashbaord Project/Finance/Invoices/2206680-moving_items 2.0.sql
+++ Dashbaord Project/Finance/Invoices/2206680-moving_items 2.0.sql
@@ -100,7 +100,7 @@ CASE
 END AS reporting_company_id,
 
 
-case when mi.date >= '2023-01-01' AND mi.date <= '2023-07-09' then 'Y_to_9_Jul' eles, 
+case when mi.date >= '2023-01-01' AND mi.date <= '2023-07-09' then 'Y_to_9_Jul' e, 
 
 
 from `erp_prod.move_items` mi
