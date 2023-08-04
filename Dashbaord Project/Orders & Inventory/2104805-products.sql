aa5522b179c3c07adaa2a80be873bef4f310c43a
Anmar Abbas <anmar@floranow.com>
2023-08-04 14:02:45 +0000

    Update query `products`

--- Dashbaord Project/Orders & Inventory/2104805-products.sql
+++ Dashbaord Project/Orders & Inventory/2104805-products.sql
@@ -172,7 +172,7 @@ when st.id in (16,17) then 'Internal - TBF'
 when st.id in (15) then 'Commission Based - Wish Flowers'
 when st.id in (304,305) then 'Commission Based - Ward'
 when st.id in (128,129,18,19,22,23,266,267,486,526,529,565,90,91,527,564) then 'Commission Based - Astra Express'
-when st.id in (165,64,569,451,450,415,414,571,570,408,411,410,572,407,406,413,412,416,417,164,165,568,573) then  case  when stg_suppliers.supplier_name = 'ASTRA Farms' then 'Commission Based - Astra Express' else 'Reselling' end ''
+when st.id in (165,64,569,451,450,415,414,571,570,408,411,410,572,407,406,413,412,416,417,164,165,568,573) then  case  when stg_suppliers.supplier_name = 'ASTRA Farms' then 'Commission Based - Astra Express' else 'Reselling' end 
 when st.id in (613,614,615,606,607,608) then 'Internal - BX Shop'
 when st.id in (616,617) then 'Internal - Wedding & Events'
 when st.id in (621,620) then 'Internal - BX DMM'
