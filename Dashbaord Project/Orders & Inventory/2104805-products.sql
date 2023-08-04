2bb4fb25bf2868d05688b94ba1b59fdb984785b6
Anmar Abbas <anmar@floranow.com>
2023-08-04 13:23:59 +0000

    Update query `products`

--- Dashbaord Project/Orders & Inventory/2104805-products.sql
+++ Dashbaord Project/Orders & Inventory/2104805-products.sql
@@ -183,7 +183,7 @@ when st.id in (522,484,567,566,531,530) then
         when stg_suppliers.supplier_name = 'ASTRA' then 'Commission Based - Astra Express'
         else 'Reselling'
         end 
-     end
+     
 
 
 else null
