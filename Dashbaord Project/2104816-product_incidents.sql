a180038b7ffb04df46d93fe0e839a9436332f13c
Anmar Abbas <anmar@floranow.com>
2023-08-04 13:44:16 +0000

    Update query `product_incidents`

--- Dashbaord Project/2104816-product_incidents.sql
+++ Dashbaord Project/2104816-product_incidents.sql
@@ -163,7 +163,7 @@ when st.id in (304,305) then 'Commission Based'
 when st.id in (128,129,18,19,22,23,266,267,486,526,529,565,90,91,527,564) then 'Commission Based'
 when st.id in (165,64,569,451,450,415,414,571,570,408,411,410,572,407,406,413,412,416,417,164,165,568,573) then 'Reselling'
 when st.id in (613,614,615,606,607,608) then 'Internal'
-when stid in (616,617) then 'Internal'
+when stoid in (616,617) then 'Internal'
 when st.id in (621,620) then 'Internal'
 when st.id in (522,484,567,566,531,530,523,485,373,372,301,300,199,198,131,130,127,126,57,56,21,20,7,6,2,1) then  case  when stg_suppliers.supplier_name = 'ASTRA Farms' then 'Commission Based' else 'Reselling' end 
 else 'Others'
