7997081eca4c14cc880d4df8481a2008602534fa
Anmar Abbas <anmar@floranow.com>
2023-08-04 13:44:23 +0000

    Update query `product_incidents`

--- Dashbaord Project/2104816-product_incidents.sql
+++ Dashbaord Project/2104816-product_incidents.sql
@@ -139,33 +139,33 @@ reseller.name as reseller,
 concat(stock.id, " - ", reseller.name , " - ", stock.name ) as full_stock_name, --stock_id
 
 case 
-when st.id in (12,13) then 'Internal - Jumeriah'
-when st.id in (10,11,618,619) then 'Internal - Spinnyes'
-when st.id in (16,17) then 'Internal - TBF'
-when st.id in (15) then 'Commission Based - Wish Flowers'
-when st.id in (304,305) then 'Commission Based - Ward'
-when st.id in (128,129,18,19,22,23,266,267,486,526,529,565,90,91,527,564) then 'Commission Based - Astra Express'
-when st.id in (165,64,569,451,450,415,414,571,570,408,411,410,572,407,406,413,412,416,417,164,165,568,573) then 'Reselling Event'
-when st.id in (613,614,615,606,607,608) then 'Internal - BX Shop'
-when st.id in (616,617) then 'Internal - Wedding & Events'
-when st.id in (621,620) then 'Internal - BX DMM'
-when st.id in (522,484,567,566,531,530,523,485,373,372,301,300,199,198,131,130,127,126,57,56,21,20,7,6,2,1) then  case  when stg_suppliers.supplier_name = 'ASTRA Farms' then 'Commission Based - Astra Express' else 'Reselling' end 
+when stid in (12,13) then 'Internal - Jumeriah'
+when stid in (10,11,618,619) then 'Internal - Spinnyes'
+when stid in (16,17) then 'Internal - TBF'
+when stid in (15) then 'Commission Based - Wish Flowers'
+when stid in (304,305) then 'Commission Based - Ward'
+when stid in (128,129,18,19,22,23,266,267,486,526,529,565,90,91,527,564) then 'Commission Based - Astra Express'
+when stid in (165,64,569,451,450,415,414,571,570,408,411,410,572,407,406,413,412,416,417,164,165,568,573) then 'Reselling Event'
+when stid in (613,614,615,606,607,608) then 'Internal - BX Shop'
+when stid in (616,617) then 'Internal - Wedding & Events'
+when stid in (621,620) then 'Internal - BX DMM'
+when stid in (522,484,567,566,531,530,523,485,373,372,301,300,199,198,131,130,127,126,57,56,21,20,7,6,2,1) then  case  when stg_suppliers.supplier_name = 'ASTRA Farms' then 'Commission Based - Astra Express' else 'Reselling' end 
      
 else 'Others'
 end as stock_model_details,
 
 case 
-when st.id in (12,13) then 'Internal'
-when st.id in (10,11,618,619) then 'Internal'
-when st.id in (16,17) then 'Internal'
-when st.id in (15) then 'Commission Based'
-when st.id in (304,305) then 'Commission Based'
-when st.id in (128,129,18,19,22,23,266,267,486,526,529,565,90,91,527,564) then 'Commission Based'
-when st.id in (165,64,569,451,450,415,414,571,570,408,411,410,572,407,406,413,412,416,417,164,165,568,573) then 'Reselling'
-when st.id in (613,614,615,606,607,608) then 'Internal'
-when st.id in (616,617) then 'Internal'
-when st.id in (621,620) then 'Internal'
-when st.id in (522,484,567,566,531,530,523,485,373,372,301,300,199,198,131,130,127,126,57,56,21,20,7,6,2,1) then  case  when stg_suppliers.supplier_name = 'ASTRA Farms' then 'Commission Based' else 'Reselling' end 
+when stid in (12,13) then 'Internal'
+when stid in (10,11,618,619) then 'Internal'
+when stid in (16,17) then 'Internal'
+when stid in (15) then 'Commission Based'
+when stid in (304,305) then 'Commission Based'
+when stid in (128,129,18,19,22,23,266,267,486,526,529,565,90,91,527,564) then 'Commission Based'
+when stid in (165,64,569,451,450,415,414,571,570,408,411,410,572,407,406,413,412,416,417,164,165,568,573) then 'Reselling'
+when stid in (613,614,615,606,607,608) then 'Internal'
+when stid in (616,617) then 'Internal'
+when stid in (621,620) then 'Internal'
+when stid in (522,484,567,566,531,530,523,485,373,372,301,300,199,198,131,130,127,126,57,56,21,20,7,6,2,1) then  case  when stg_suppliers.supplier_name = 'ASTRA Farms' then 'Commission Based' else 'Reselling' end 
 else 'Others'
 end as stock_model,
 
