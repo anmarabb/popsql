9882431898a775fb544b98539b25edea97f018de
Anmar Abbas <anmar@floranow.com>
2023-08-04 11:37:33 +0000

    Update query `products`

--- Dashbaord Project/Orders & Inventory/2104805-products.sql
+++ Dashbaord Project/Orders & Inventory/2104805-products.sql
@@ -10,7 +10,7 @@ stg_suppliers.supplier_name as supplier,
 
 case 
 when li.parent_line_item_id is not null then parent_li_suppliers.supplier_name 
-when stg_users.financial_administration = 'Bulk' then  ii.meta_data.supplier
+--when stg_users.financial_administration = 'Bulk' then  ii.meta_data.supplier
 --when ii.meta_data.supplier_name is null then li_suppliers.supplier_name else ii.meta_data.supplier_name
 else li_suppliers.supplier_name 
 end as supplier_name,
