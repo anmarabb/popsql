c45ca9fc86dccaa406b402d406b3ccf991d8cea4
Anmar Abbas <anmar@floranow.com>
2023-08-05 11:34:40 +0000

    Update query `MKP - (JDBC) - Full Refresh`

--- Databricks/2226748-MKP - (JDBC) - Full Refresh.sql
+++ Databricks/2226748-MKP - (JDBC) - Full Refresh.sql
@@ -95,3 +95,94 @@ with ThreadPoolExecutor(max_workers=num_threads) as pool:
 /*
 calculates and compares the row counts for the source (PostgreSQL) and destination (Databricks Delta Lake) tables. The output should help you verify that the ETL process was successful.
 */
+
+from pyspark.sql import SparkSession
+
+# Create Spark Session
+spark = SparkSession.builder.getOrCreate()
+
+# Database configuration
+# Database configuration
+driver = "org.postgresql.Driver"
+database_host = "prod-floranow.cluster-ro-c47qrxmyb1ht.eu-central-1.rds.amazonaws.com"
+database_port = "5432"
+database_name = "marketplace_prod"
+user = "readonly"
+password = "UfnpdeKIxseRwUsYzkKm"
+
+url = f"jdbc:postgresql://{database_host}:{database_port}/{database_name}"
+
+# Function to sanitize column names (remove special characters)
+def sanitize_column_name(name):
+    return re.sub(r'[^A-Za-z0-9_]', '_', name)
+
+# Query to fetch all table names from the 'public' schema
+query = """
+    SELECT table_schema || '.' || table_name AS full_rel_name 
+    FROM information_schema.tables 
+    WHERE table_schema = 'public'
+"""
+
+# Get the list of all tables
+table_list = (spark.read
+    .format("jdbc")
+    .option("driver", driver)
+    .option("url", url)
+    .option("query", query)
+    .option("user", user)
+    .option("password", password)
+    .load()
+    .rdd
+    .flatMap(lambda x: x)
+    .collect())
+
+# Function to fetch row count from PostgreSQL
+def get_postgresql_row_count(table_name):
+    query = f"SELECT COUNT(*) as count FROM {table_name}"
+    row_count = (spark.read
+        .format("jdbc")
+        .option("driver", driver)
+        .option("url", url)
+        .option("query", query)
+        .option("user", user)
+        .option("password", password)
+        .load()
+        .collect()[0]["count"])
+    return row_count
+
+# Function to fetch row count from Databricks Delta Lake
+def get_databricks_row_count(table_name):
+    delta_table = spark.table(table_name)
+    row_count = delta_table.count()
+    return row_count
+
+# Initialize lists for good and check tables
+good_tables = []
+check_tables = []
+
+# Iterate over the tables
+for table_name in table_list:
+    if table_name not in ['public.friendly_id_slugs']:
+        postgresql_table_name = table_name
+        databricks_table_name = table_name.replace('public.', 'fn_sources.mkp.')
+        try:
+            postgresql_row_count = get_postgresql_row_count(postgresql_table_name)
+            databricks_row_count = get_databricks_row_count(databricks_table_name)
+            
+            if postgresql_row_count == databricks_row_count:
+                good_tables.append((table_name, postgresql_row_count))
+            else:
+                diff = abs(postgresql_row_count - databricks_row_count)
+                check_tables.append((table_name, postgresql_row_count, databricks_row_count, diff))
+            
+        except Exception as e:
+            print(f"An error occurred with table {table_name}: {e}")
+
+# Print the results
+print("Good Tables:")
+for table_name, row_count in good_tables:
+    print(f"- {table_name} - Source: {row_count}, Databricks: {row_count}")
+    
+print("\nCheck Tables:")
+for table_name, postgresql_row_count, databricks_row_count, diff in check_tables:
+    print(f"- {table_name} - Source: {postgresql_row_count}, Databricks: {databricks_row_count}, diff: {diff}")
