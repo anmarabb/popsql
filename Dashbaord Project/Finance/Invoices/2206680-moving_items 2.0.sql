1b98d3eee91995c0716e64fd8cb5c43d721060bf
Anmar Abbas <anmar@floranow.com>
2023-08-05 19:44:52 +0000

    Update query `moving_items 2.0`

--- Dashbaord Project/Finance/Invoices/2206680-moving_items 2.0.sql
+++ Dashbaord Project/Finance/Invoices/2206680-moving_items 2.0.sql
@@ -7,7 +7,6 @@ create or replace table `floranow.Floranow_ERP.move_items` as
 
 select 
 
-case when mi.entry_type = 'DEBIT' then (case when  mi.residual<0 then 0 else mi.residual end) else (case when mi.residual >0 then 0 else mi.residual  end) end as residual,
 mi.balance,
 
 mi.residual as raw_residual,
