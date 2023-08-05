2fcf33f25c7f25a49a9df74364a9cbe6b116cadb
Anmar Abbas <anmar@floranow.com>
2023-08-05 19:16:58 +0000

    Update query `moving_items 2.0`

--- Dashbaord Project/Finance/Invoices/2206680-moving_items 2.0.sql
+++ Dashbaord Project/Finance/Invoices/2206680-moving_items 2.0.sql
@@ -1,7 +1,7 @@
 create or replace table `floranow.Floranow_ERP.move_items` as
 
  sum(case when date_diff( cast(current_date() as date ),cast(mi.aging_date as date), DAY) <= 30 then mi.balance else 0 end) as up_to_30_days,
-gri
+grio
 
 select 
 customer.name as Customer,
