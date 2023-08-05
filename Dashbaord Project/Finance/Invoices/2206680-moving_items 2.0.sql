f9874e7df906b658676ddb0d600e4dde9684d091
Anmar Abbas <anmar@floranow.com>
2023-08-05 19:06:29 +0000

    Update query `moving_items 2.0`

--- Dashbaord Project/Finance/Invoices/2206680-moving_items 2.0.sql
+++ Dashbaord Project/Finance/Invoices/2206680-moving_items 2.0.sql
@@ -68,7 +68,7 @@ mi.reconciled,
 
 mi.residual as raw_residual,
 
-case when mi.entry_type = 'DEBIT' then (case when  mi.residual<0 then 0 else mi.residual end) else (case when mi.residual >0) end as residual,
+case when mi.entry_type = 'DEBIT' then (case when  mi.residual<0 then 0 else mi.residual end) else (case when mi.residual >0 ) end as residual,
 
 
 
