d2a2e019fd89f826edb054231580f66d129fd427
Anmar Abbas <anmar@floranow.com>
2023-08-05 19:44:04 +0000

    Update query `moving_items 2.0`

--- Dashbaord Project/Finance/Invoices/2206680-moving_items 2.0.sql
+++ Dashbaord Project/Finance/Invoices/2206680-moving_items 2.0.sql
@@ -10,7 +10,6 @@ select
 
 mi.balance,
 mi.residual as raw_residual,
-case when mi.entry_type = 'DEBIT' then (case when  mi.residual<0 then 0 else mi.residual end) else (case when mi.residual >0 then 0 else mi.residual  end) end as residual,
 
 
 case when i.id is not null then date(i.due_date) else date(mi.date) end as aging_date,
