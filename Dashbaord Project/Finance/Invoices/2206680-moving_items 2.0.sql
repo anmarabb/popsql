5ec9117e0769be5b6cd89260f525d667d4ccd220
Anmar Abbas <anmar@floranow.com>
2023-08-05 19:11:57 +0000

    Update query `moving_items 2.0`

--- Dashbaord Project/Finance/Invoices/2206680-moving_items 2.0.sql
+++ Dashbaord Project/Finance/Invoices/2206680-moving_items 2.0.sql
@@ -106,7 +106,7 @@ END AS reporting_company_id,
 
 case when mi.date >= '2023-01-01' AND mi.date <= '2023-07-09' then 'Y_to_9_Jul' else null end as Y_to_9_Jul, 
 
-case when i.id is not null then i.d
+case when i.id is not null then i.deu
 
 from `erp_prod.move_items` mi
 join `erp_prod.users` customer on mi.user_id = customer.id
