a19fdcf6d6f120538ac028472ce11389097eba9f
Anmar Abbas <anmar@floranow.com>
2023-08-06 02:21:55 +0000

    Update query `Move Items - Abi`

--- Dashbaord Project/Finance/Paymnets/2104804-Move Items - Abi.sql
+++ Dashbaord Project/Finance/Paymnets/2104804-Move Items - Abi.sql
@@ -66,7 +66,7 @@ left join `floranow.erp_prod.manageable_accounts` manageable_accounts on custome
 left join `floranow.erp_prod.account_managers`  am on manageable_accounts.account_manager_id = am.id
 left join `floranow.erp_prod.users`  as amu on am.user_id = amu.id
     left join `floranow.erp_prod.financial_administrations`  fad on customer.financial_administration_id = fad.id
-    left join `floranow.erp_prod.warehouses`  warehouse  on customer.warehouse_id = warehouse.id
+left join `floranow.erp_prod.warehouses`  warehouse  on customer.warehouse_id = warehouse.id
 
 
 
