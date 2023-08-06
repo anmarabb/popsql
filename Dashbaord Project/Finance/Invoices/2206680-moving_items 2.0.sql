72d097cb1f6e8fa88805aff55d9f6114911d1a8e
Anmar Abbas <anmar@floranow.com>
2023-08-06 13:02:11 +0000

    Update query `moving_items 2.0`

--- Dashbaord Project/Finance/Invoices/2206680-moving_items 2.0.sql
+++ Dashbaord Project/Finance/Invoices/2206680-moving_items 2.0.sql
@@ -38,7 +38,7 @@ stg_move_items.aging_date,
 
 customer.name as Customer,
 customer.debtor_number,
-customer.id as c,
+customer.id as customer_id,
 customer.financial_administration_id,
 w.name as warehouses,
 
