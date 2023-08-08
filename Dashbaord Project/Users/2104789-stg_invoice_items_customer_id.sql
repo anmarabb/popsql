baeba83764ee0dc4cef3bda4c54fa7a81ad49491
Anmar Abbas <anmar@floranow.com>
2023-08-08 12:09:18 +0000

    Update query `stg_invoice_items_customer_id`

--- Dashbaord Project/Users/2104789-stg_invoice_items_customer_id.sql
+++ Dashbaord Project/Users/2104789-stg_invoice_items_customer_id.sql
@@ -99,7 +99,7 @@ SAFE_DIVIDE(abs(sum(case when i.invoice_type = 1 then ii.price_without_tax else 
 
 ii.customer_id as customer_id ,
 
-count (distinct case when date_diff(date(i.printed_at) , current_date() , MONTH) = 0 then ii.invoice_id else 0 end) as MTD_orders,
+count (distinct case when date_diff(date(i.printed_at) , current_date() , MONTH) = 0 then ii.invoice_id else nu end) as MTD_orders,
 
 
 
