3c5c6fc1fc489dd7e594f34ca1bfceb1144a85e6
Anmar Abbas <anmar@floranow.com>
2023-08-08 14:04:25 +0000

    Update query `moving_items 2.0`

--- Dashbaord Project/Finance/Invoices/2206680-moving_items 2.0.sql
+++ Dashbaord Project/Finance/Invoices/2206680-moving_items 2.0.sql
@@ -140,7 +140,7 @@ END AS reporting_company_id,
 --case when mi.date >= '2023-01-01' AND mi.date <= '2023-07-09' then 'Y_to_9_Jul' else null end as Y_to_9_Jul, 
 
 
-case  when round(sum(stg_move_items.residual) over ()) != round(sum(mi.balance) over, 2) then 'cheak' else null end as Ledger_SOA_cheack,
+case  when round(sum(stg_move_items.residual) over ()) != round(sum(mi.balance) over (_), 2) then 'cheak' else null end as Ledger_SOA_cheack,
 
 
 
