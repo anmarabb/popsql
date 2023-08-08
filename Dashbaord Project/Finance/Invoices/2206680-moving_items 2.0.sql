2f5193f044b552e5458be4577426553a28f50ccf
Anmar Abbas <anmar@floranow.com>
2023-08-08 14:03:16 +0000

    Update query `moving_items 2.0`

--- Dashbaord Project/Finance/Invoices/2206680-moving_items 2.0.sql
+++ Dashbaord Project/Finance/Invoices/2206680-moving_items 2.0.sql
@@ -141,7 +141,7 @@ END AS reporting_company_id,
 
 
 case 
-    when  != round(sum(mi.balance), 2) then 'cheak'
+    when sum != round(sum(mi.balance), 2) then 'cheak'
     else null end as Ledger_SOA_cheack,
 
 
