70ffbb2217bbbae1ca674385daf09945d5376f55
Anmar Abbas <anmar@floranow.com>
2023-08-08 12:12:57 +0000

    Update query `stg_invoice_items_customer_id`

--- Dashbaord Project/Users/2104789-stg_invoice_items_customer_id.sql
+++ Dashbaord Project/Users/2104789-stg_invoice_items_customer_id.sql
@@ -103,7 +103,7 @@ count (distinct case when date_diff(date(i.printed_at) , current_date() , MONTH)
 count (distinct case when date_diff(current_date(),date(i.printed_at), MONTH) = 1 then ii.invoice_id else null end) as m_1_orders,
 count (distinct case when date_diff(current_date(),date(i.printed_at), MONTH) = 2 then ii.invoice_id else null end) as m_2_orders,
 count (distinct case when date_diff(current_date(),date(i.printed_at), MONTH) = 3 then ii.invoice_id else null end) as m_3_orders,
-count (case when date_diff(current_date(),date(i.printed_at), YEAR) = 0 then ii.invoice_id else null end) as ytd_orders,
+count (distinct case when date_diff(current_date(),date(i.printed_at), YEAR) = 0 then ii.invoice_id else null end) as ytd_orders,
 
 
 
