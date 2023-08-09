ea38efd5b0bb3869976fa4ed1bc818cee7a97a5d
Anmar Abbas <anmar@floranow.com>
2023-08-09 11:28:07 +0000

    Update query `Users`

--- Dashbaord Project/Users/2104778-Users.sql
+++ Dashbaord Project/Users/2104778-Users.sql
@@ -308,5 +308,5 @@ left join `floranow.erp_prod.payment_terms` as pt on pt.id = u.payment_term_id
 left join `floranow.erp_prod.financial_administrations` as f on f.id = u.financial_administration_id
 left join `floranow.erp_prod.warehouses`  w on u.warehouse_id = w.id
 --where u.email not like '%fake.com%' 
-where 
+where =
 group by 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,master_account,route_name,city_Upper,user_link,company_name,warehouses,MTD_holland,LMTD_holland,m_1_holland,m_2_holland,m_3_holland,ytd_holland,wtd_holland,w_1_holland,w_2_holland,w_3_holland,source_system
