0bfee95531b64bc131c02b228facb77c7fea0fc4
Anmar Abbas <anmar@floranow.com>
2023-08-23 12:41:04 +0000

    Update query `invoices_items`

--- Dashbaord Project/Finance/Invoices/2104766-invoices_items.sql
+++ Dashbaord Project/Finance/Invoices/2104766-invoices_items.sql
@@ -10,7 +10,7 @@ case
     when w.name in ('Jeddah Warehouse') then 'Mahmoud'
     when w.name in ('Tabuk Warehouse') then 'Majed'
     when w.name in ('Medina Warehouse') then 'Abd Alaziz'
-    else null
+    else null end as 
 
 
 
