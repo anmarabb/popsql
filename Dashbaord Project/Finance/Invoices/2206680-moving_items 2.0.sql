755326274794798ffdcdbe63b79479af711b486d
Anmar Abbas <anmar@floranow.com>
2023-08-23 10:18:33 +0000

    Update query `moving_items 2.0`

--- Dashbaord Project/Finance/Invoices/2206680-moving_items 2.0.sql
+++ Dashbaord Project/Finance/Invoices/2206680-moving_items 2.0.sql
@@ -23,7 +23,7 @@ w.name as warehouses,
 case --financial ID
 when mi.financial_administration_id = 1 then 'KSA'
 when mi.financial_administration_id = 2 then 'UAE'
-when customer.financial_administration_id = 3 then 'Jordan'
+when m.financial_administration_id = 3 then 'Jordan'
 when customer.financial_administration_id = 4 then 'kuwait'
 when customer.financial_administration_id = 5 then 'Qatar'
 when customer.financial_administration_id = 6 then 'Bulk'
