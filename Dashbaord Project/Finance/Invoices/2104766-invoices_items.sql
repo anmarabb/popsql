b43762670cafcda50189df2ece6739843b7a7df3
Anmar Abbas <anmar@floranow.com>
2023-08-25 06:27:50 +0000

    Update query `invoices_items`

--- Dashbaord Project/Finance/Invoices/2104766-invoices_items.sql
+++ Dashbaord Project/Finance/Invoices/2104766-invoices_items.sql
@@ -7,7 +7,7 @@ SELECT
 
 case 
 when stg_users.customer like '%Tamimi%' THEN 'Tamimi'
-when stg_users.customer like '%Tamimi%' THEN 'Tamimi'
+when stg_users.customer like '%TAMIMI%' THEN 'Tamimi'
 when stg_users.customer like '%tamimi%' THEN 'Tamimi'
 
 
