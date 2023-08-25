d12b418f2e55aa72ecd177fdd40ed7542bc70863
Anmar Abbas <anmar@floranow.com>
2023-08-25 06:30:01 +0000

    Update query `invoices_items`

--- Dashbaord Project/Finance/Invoices/2104766-invoices_items.sql
+++ Dashbaord Project/Finance/Invoices/2104766-invoices_items.sql
@@ -9,7 +9,7 @@ case
 when stg_users.customer like '%Tamimi%' THEN 'Tamimi'
 when stg_users.customer like '%TAMIMI%' THEN 'Tamimi'
 when stg_users.customer like '%tamimi%' THEN 'Tamimi'
-when stg_users.customer in ('REMA1','REMA2','REMA3','REMA4','REMA5','REMA6','REMA7','REMA8') then 'Astra Shop' else 'null' end as astra_shop,
+when stg_users.customer in ('REMA1','REMA2','REMA3','REMA4','REMA5','REMA6','REMA7','REMA8') then 'Astra Shop'\ else 'null' end as astra_shop,
 
 
 case 
