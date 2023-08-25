f82bcb169d12796d64b59bcba34041086c99c0a2
Anmar Abbas <anmar@floranow.com>
2023-08-25 06:35:22 +0000

    Update query `invoices_items`

--- Dashbaord Project/Finance/Invoices/2104766-invoices_items.sql
+++ Dashbaord Project/Finance/Invoices/2104766-invoices_items.sql
@@ -17,7 +17,7 @@ case
 when astra_shop = 'Astra Shop' and sales_source = 'Astra' then 'Astra Shop Sales From Astra Products' 
 when astra_shop = 'Astra Shop' and sales_source = 'Non Astra' then 'Astra Shop Sales From Non Astra Prudyct' 
 
-else null end as astra_shop_sales Type
+else null end as astra_shop_sales type
 
 
 case 
