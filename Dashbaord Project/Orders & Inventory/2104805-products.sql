f587ecac4884c89bcc312a26bd72219741a554da
Anmar Abbas <anmar@floranow.com>
2023-09-21 05:34:01 +0000

    Update query `products`

--- Dashbaord Project/Orders & Inventory/2104805-products.sql
+++ Dashbaord Project/Orders & Inventory/2104805-products.sql
@@ -344,7 +344,7 @@ case
     when p.product_name like '%Foam%' THEN 'Accessories'
     when p.product_name like '%Follie%' THEN 'Accessories'
     when p.product_name like '%Glue%' THEN 'Accessories'
-    when p.product_name like '%Golden%' THEN 'Accessories'
+    --when p.product_name like '%Golden%' THEN 'Accessories'
     --when p.product_name like '%Green%' THEN 'Accessories'
     when p.product_name like '%Ideal%' THEN 'Accessories'
     when p.product_name like '%Kash%' THEN 'Accessories'
