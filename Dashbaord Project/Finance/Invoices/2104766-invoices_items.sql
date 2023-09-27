c690d6457a85ec20d18c00ca0735c22e9f529583
Anmar Abbas <anmar@floranow.com>
2023-09-27 10:52:21 +0000

    Update query `invoices_items`

--- Dashbaord Project/Finance/Invoices/2104766-invoices_items.sql
+++ Dashbaord Project/Finance/Invoices/2104766-invoices_items.sql
@@ -565,6 +565,9 @@ CASE
     ELSE 0 
   END as delivery_charge_amount,
 
+
+
+
 case when i.delivery_charge_amount > 0 then 'Yes' else 'No' end as delivery_charge_applied,
 --li.delivery_charge.amount as delivery_charge_amount,
 
