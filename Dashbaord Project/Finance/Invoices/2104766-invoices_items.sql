463ca77f9bb62ab704733e64ea8727801896b7ac
Anmar Abbas <anmar@floranow.com>
2023-09-27 10:09:21 +0000

    Update query `invoices_items`

--- Dashbaord Project/Finance/Invoices/2104766-invoices_items.sql
+++ Dashbaord Project/Finance/Invoices/2104766-invoices_items.sql
@@ -559,7 +559,7 @@ i.source_system,
 routes.name as routes,
 i.delivery_charge_amount,
 
-case when i.delivery_charge_amount > 0 then 'delivery_charge' 
+case when i.delivery_charge_amount > 0 then 'delivery_charge' else 
 --li.delivery_charge.amount as delivery_charge_amount,
 
 
