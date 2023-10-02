c37d1e75df65ffa5e72a714c2de490fc9f6c2158
Anmar Abbas <anmar@floranow.com>
2023-10-02 16:31:56 +0000

    Update query `invoices_items`

--- Dashbaord Project/Finance/Invoices/2104766-invoices_items.sql
+++ Dashbaord Project/Finance/Invoices/2104766-invoices_items.sql
@@ -329,7 +329,7 @@ when ii.meta_data.supplier_name in ('Verdissimo - AWS','The Orchid garden Resell
 when li_suppliers.supplier_name in ('Ward Flowers') and fs.name in ('Ward Flower Inventory') then 'Trading'
 
 else 'Normal'
-end as  v,
+end as trading_funcation,
 
 case 
 when 
