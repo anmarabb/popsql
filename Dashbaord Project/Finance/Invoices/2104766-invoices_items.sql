bde28aa892c0121df90b17fb6ba9674defe214be
Anmar Abbas <anmar@floranow.com>
2023-10-09 11:37:53 +0000

    Update query `invoices_items`

--- Dashbaord Project/Finance/Invoices/2104766-invoices_items.sql
+++ Dashbaord Project/Finance/Invoices/2104766-invoices_items.sql
@@ -314,8 +314,8 @@ when ii.meta_data.supplier_name in ('Verdissimo - AWS','The Orchid garden Resell
 when li_suppliers.supplier_name in ('Floranow Holland') and fs.name in ('Holland Reselling','Holland Reselling Dammam','Holland Reselling Riyadh') then 'Re-Selling'
 when li_suppliers.supplier_name in ('Floranow Flash sale') and fs.name in ('Floranow Express Flash sale', 'Floranow Flash Sale Dammam','Floranow Flash Sale Riyadh','Floranow Flash Sale Tabuk', 'Floranow Flash Sale Jeddah') then 'Re-Selling'
 when li_suppliers.supplier_name in ('wish flower') then 'Re-Selling'
-when li_suppliers.supplier_name in ('ASTRA Farms') and fs.name in ('Astra DXB out') then 'Marketplace'
-when ii.meta_data.supplier_name in ('ASTRA Farms') and fs.name in ('Astra DXB out') then 'Marketplace'
+when li_suppliers.supplier_name in ('ASTRA Farms') and fs.name in ('Astra DXB out') then 'Commission Based'
+when ii.meta_data.supplier_name in ('ASTRA Farms') and fs.name in ('Astra DXB out') then 'Commission Based'
 when li_suppliers.supplier_name in ('Ward Flowers') and fs.name in ('Ward Flower Inventory') then 'Commission Based'
 else 'Pre-Selling'
 end as trading_model,
