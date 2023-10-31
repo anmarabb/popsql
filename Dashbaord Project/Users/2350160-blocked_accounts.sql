99d5d2c4705a2fb1d7b159bdcdfcd007a2d676e1
Anmar Abbas <anmar@floranow.com>
2023-10-31 13:59:18 +0000

    Update query `blocked_accounts`

--- Dashbaord Project/Users/2350160-blocked_accounts.sql
+++ Dashbaord Project/Users/2350160-blocked_accounts.sql
@@ -1,4 +1,4 @@
-create or replace table `floranow.Floranow_ERP.users` as blocked_account
+create or replace table `floranow.Floranow_ERP.users` as 
 
 WITH subquery_cte AS (
     SELECT invoices.customer_id, SUM(move_items.residual) AS total_residual
