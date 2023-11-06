99d4920d4afe1bb9914630771b4bab5436bfe520
Anmar Abbas <anmar@floranow.com>
2023-11-06 20:46:09 +0000

    Update query `Move Items - Abi`

--- Dashbaord Project/Finance/Paymnets/2104804-Move Items - Abi.sql
+++ Dashbaord Project/Finance/Paymnets/2104804-Move Items - Abi.sql
@@ -78,7 +78,7 @@ UNION ALL ------------------------------------query for unreconciled payment tra
 
 select date(cmi.date)  as cridet_date,
 warehouse.name as warehouse,
-       customer.warehouse_id as customer_warehouse_id,
+customer.warehouse_id as customer_warehouse_id,
            date(pt.payment_received_at)                                    payment_received_at,
            customer.name                                                   customer,
            customer.debtor_number                                          debtor_number,
