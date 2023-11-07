ecd09f2366e9a49bb9965ed6339f2fc836ebbbc8
Anmar Abbas <anmar@floranow.com>
2023-11-07 09:38:55 +0000

    Update query `Move Items - Abi`

--- Dashbaord Project/Finance/Paymnets/2104804-Move Items - Abi.sql
+++ Dashbaord Project/Finance/Paymnets/2104804-Move Items - Abi.sql
@@ -58,6 +58,7 @@ inv.printed_at,
 case when pt.payment_gateway=0 then 'telr' else null end as payment_gateway,
 
 
+
 from `floranow.erp_prod.payments` payments
 join `floranow.erp_prod.users`  customer on payments.user_id = customer.id
 join `floranow.erp_prod.move_items`  dmi on payments.debit_move_item_id = dmi.id
