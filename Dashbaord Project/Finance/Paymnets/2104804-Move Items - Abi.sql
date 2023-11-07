31dd34aff776069cb9e58634cda1526989cb028b
Anmar Abbas <anmar@floranow.com>
2023-11-07 09:23:39 +0000

    Update query `Move Items - Abi`

--- Dashbaord Project/Finance/Paymnets/2104804-Move Items - Abi.sql
+++ Dashbaord Project/Finance/Paymnets/2104804-Move Items - Abi.sql
@@ -2,7 +2,7 @@ create or replace table `floranow.Floranow_ERP.payments` as
 
     
     select 
-    case when pt.payment_received_at is not null the
+    case when pt.payment_received_at is not null then 
     date(cmi.date) as cridet_date, -- date of paymnet_transaction
      warehouse.name as warehouse,
 
