487b027a6406a5ed0632c347545e7549b5475b41
Anmar Abbas <anmar@floranow.com>
2023-11-09 05:41:33 +0000

    Update query `users`

--- Dashbaord Project/Users/2104778-users.sql
+++ Dashbaord Project/Users/2104778-users.sql
@@ -293,7 +293,7 @@ stg_invoices.source_system,
 
 w.name as warehouses,
 case when u.email  like '%fake.com%' then 'fake.com' else 'normal' end as fake_email,
- case when u.email  like '%fake_%' or u.email  like '%temp_%'
+ case when u.email  like '%fake_%' or u.email  like '%temp_%' then 'fake.com'
 
 
 
