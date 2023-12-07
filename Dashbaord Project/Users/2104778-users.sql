1ed036c1b7b2d2c22e544efd9e64d218bcda9294
Anmar Abbas <anmar@floranow.com>
2023-12-07 17:09:04 +0000

    Update query `users`

--- Dashbaord Project/Users/2104778-users.sql
+++ Dashbaord Project/Users/2104778-users.sql
@@ -20,7 +20,7 @@ c.country_name as country,
 case 
 when u.order_blocked_status = 0 then 'Unblocked'
 when u.order_blocked_status = 1 then 'Manually Blocked'
-when order_blocked_status = 2 then 'Exceeded Credit Limit'
+when uorder_blocked_status = 2 then 'Exceeded Credit Limit'
 when order_blocked_status = 3 then 'Overdue Invoices'
 else null end as order_blocked_status,
 
