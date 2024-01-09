f4052d723c0b7344698f75ab086304ff553d9b08
Anmar Abbas <anmar@floranow.com>
2024-01-09 09:24:18 +0000

    Update query `Move Items - Abi`

--- Dashbaord Project/Finance/Paymnets/2104804-Move Items - Abi.sql
+++ Dashbaord Project/Finance/Paymnets/2104804-Move Items - Abi.sql
@@ -140,6 +140,7 @@ join `floranow.erp_prod.users`  customer on cmi.user_id = customer.id
 left join `floranow.erp_prod.payment_transactions`  pt on cmi.documentable_id = pt.id and cmi.documentable_type = 'PaymentTransaction' and cmi.entry_type = 'CREDIT'
 
 left join  `floranow.erp_prod.invoices` cn on cmi.documentable_id = cn.id and cmi.documentable_type = 'Invoice' and cmi.entry_type = 'CREDIT'
+
 left join `floranow.erp_prod.user_categories`  category on customer.user_category_id = category.id
 left join `floranow.erp_prod.manageable_accounts`  manageable_accounts on customer.id = manageable_accounts.manageable_id and manageable_accounts.manageable_type = 'User'
 left join `floranow.erp_prod.account_managers`  am on manageable_accounts.account_manager_id = am.id
