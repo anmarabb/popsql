c945e06ac9b08b92372042cea8fd718a3df626ff
Anmar Abbas <anmar@floranow.com>
2024-01-10 05:59:06 +0000

    Update query `Move Items - Abi`

--- Dashbaord Project/Finance/Paymnets/2104804-Move Items - Abi.sql
+++ Dashbaord Project/Finance/Paymnets/2104804-Move Items - Abi.sql
@@ -161,9 +161,9 @@ left join `floranow.erp_prod.move_items`  PTmi on  PTmi.id = cmi.id and PTmi.doc
 where 
     round(cmi.residual, 2) != 0 
     and cmi.balance != 0
-    and cmi.deleted_at is null
-    and cmi.documentable_id is not null
     
+    and cmi.documentable_id is not null
+
     and cmi.__hevo__marked_deleted is not true
     
     and cmi.entry_type='CREDIT'
