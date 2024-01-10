3b29652ffc053ae660735cc2517b45d6db2b3898
Anmar Abbas <anmar@floranow.com>
2024-01-10 05:56:23 +0000

    Update query `Move Items - Abi`

--- Dashbaord Project/Finance/Paymnets/2104804-Move Items - Abi.sql
+++ Dashbaord Project/Finance/Paymnets/2104804-Move Items - Abi.sql
@@ -162,7 +162,6 @@ where
 round(cmi.residual, 2) != 0 
 and cmi.entry_type='CREDIT'
 and cmi.balance != 0
-
 and cmi.deleted_at is null
 and cmi.documentable_id is not null
 and cmi.__hevo__marked_deleted is not true
