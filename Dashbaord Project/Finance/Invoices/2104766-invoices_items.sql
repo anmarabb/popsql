f26a15384cbe978f00c9cdddb219f89c7559d324
Anmar Abbas <anmar@floranow.com>
2024-01-17 00:17:20 +0000

    Update query `invoices_items`

--- Dashbaord Project/Finance/Invoices/2104766-invoices_items.sql
+++ Dashbaord Project/Finance/Invoices/2104766-invoices_items.sql
@@ -523,7 +523,7 @@ when ii.product_name like '%Eustoma%' THEN 'Eustoma'
 when ii.product_name like '%Gerbera%' THEN 'Gerbera'
 when ii.product_name like '%Spray Rose%' THEN 'Spray-Rose'
 when ii.product_name like '%Trachelium%' THEN 'Trachelium'
-when ii.product_name like '%Aster Solidago Frida%' THEN 'Trachelium'
+when ii.product_name like '%Aster Solidago Frida%' THEN 'Solidago'
 
 when li.category is null then INITCAP(ii.category) 
 else INITCAP(li.category) 
